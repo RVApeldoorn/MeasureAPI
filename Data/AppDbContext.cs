8404c1e50e96c0bcd51023a339709c0c68070e90
Rob <vanapeldoorn.r@gmail.com>
2025-04-16 11:37:29 +0200

    [1] Updated models and seperated measurements

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,7 +5,11 @@ namespace MeasurementApi.Data;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<MeasurementType> MeasurementTypes { get; set; }
+    public DbSet<MeasurementValue> MeasurementValues { get; set; }
+    public DbSet<MeasurementRequest> MeasurementRequests { get; set; }
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<Measurement> Measurements => Set<Measurement>();
 }
---
+++ Models/MeasurementType.cs
@@ -0,0 +1,8 @@
+namespace MeasurementApi.Models;
+
+public class MeasurementType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Unit { get; set; } = string.Empty; 
+}
