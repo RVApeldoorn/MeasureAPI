b19d1fa3112838ea3176eb42ca8e1871e02561de
David de Ruiter <david.deruiter@hotmail.com>
2025-04-23 11:54:12 +0200

    Change seed

--- Data/DbSeeder.cs
+++ Data/DbSeeder.cs
@@ -34,8 +34,8 @@ namespace MeasurementApi.Data
             if(!context.Auths.Any())
             {
                 context.Auths.AddRange(
-                    new Auth {Id = 1, PatientId = 1, AuthKey = "123456" },
-                    new Auth {Id = 2, PatientId = 2, AuthKey = "abcdef" }
+                    new Auth {Id = 1, PatientId = 1, AuthKey = "12345" },
+                    new Auth {Id = 2, PatientId = 2, AuthKey = "98765" }
                 );
             }
 
