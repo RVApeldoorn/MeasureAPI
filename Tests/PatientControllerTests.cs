866ee0597cc60277cba2adcd8377b7304f35a5a1
David de Ruiter <david.deruiter@hotmail.com>
2025-05-10 20:04:42 +0200

    Init unittesten

--- Tests/PatientControllerTests.cs
+++ Tests/PatientControllerTests.cs
@@ -6,7 +6,6 @@ using MeasurementApi.Controllers;
 using MeasurementApi.DTOs;
 using MeasurementApi.Services.Interfaces;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Http;
 
 public class PatientControllerTests
 {
@@ -165,6 +164,140 @@ public class PatientControllerTests
         _serviceMock.Setup(s => s.SubmitMeasurement(Patient1Id, dto)).Returns(Task.CompletedTask);
 
         var result = await _controller.SubmitMeasurements(dto);
+
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task SubmitMeasurements_ReturnsBadRequest_WhenPatientIdIsMissing()
+    {
+        var dto = new MeasurementSubmissionDto
+        {
+            sessionId = 1,
+            Values = new List<MeasurementValueDto>
+            {
+                new MeasurementValueDto { Value = 1 },
+                new MeasurementValueDto { Value = 2 }
+            }
+        };
+
+        // Remove the patient ID from the claims
+        _controller.ControllerContext.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity());
+
+        var result = await _controller.SubmitMeasurements(dto);
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+
+    [Fact]
+    public async Task SubmitMeasurements_ReturnsBadRequest_WhenSessionIdIsInvalid()
+    {
+        var dto = new MeasurementSubmissionDto
+        {
+            sessionId = -1,
+            Values = new List<MeasurementValueDto>
+            {
+                new MeasurementValueDto { Value = 1 },
+                new MeasurementValueDto { Value = 2 }
+            }
+        };
+
+        var result = await _controller.SubmitMeasurements(dto);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task SubmitMeasurements_ReturnsBadRequest_WhenSessionIdIsNotFound()
+    {
+        var dto = new MeasurementSubmissionDto
+        {
+            sessionId = 999,
+            Values = new List<MeasurementValueDto>
+            {
+                new MeasurementValueDto { Value = 1 },
+                new MeasurementValueDto { Value = 2 }
+            }
+        };
+
+        _serviceMock.Setup(s => s.SubmitMeasurement(Patient1Id, dto))
+                    .ThrowsAsync(new Exception("Session not found"));
+
+        var result = await _controller.SubmitMeasurements(dto);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetSessions_ReturnsOk_WhenSessionsExist()
+    {
+        var sessions = new List<MeasurementSessionOverviewDto>
+        {
+            new MeasurementSessionOverviewDto
+            {
+                SessionId = 1,
+                DueDate = DateTime.Now.AddDays(1),
+                IsCompleted = false,
+                Requests = new List<MeasurementRequestDto>()
+            }
+        };
+
+        _serviceMock.Setup(s => s.GetSessionsByPatient(Patient1Id)).ReturnsAsync(sessions);
+
+        var result = await _controller.GetSessions();
+
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetSessions_ReturnsNotFound_WhenNoSessionsExist()
+    {
+        _serviceMock.Setup(s => s.GetSessionsByPatient(Patient1Id)).ReturnsAsync(new List<MeasurementSessionOverviewDto>());
+
+        var result = await _controller.GetSessions();
+
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetSessions_ReturnsUnauthorized_WhenPatientIdIsMissing()
+    {
+        // Remove the patient ID from the claims
+        _controller.ControllerContext.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity());
+
+        var result = await _controller.GetSessions();
+
+        result.Should().BeOfType<UnauthorizedObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetSessions_ReturnsBadRequest_WhenServiceThrows()
+    {
+        _serviceMock.Setup(s => s.GetSessionsByPatient(Patient1Id))
+                    .ThrowsAsync(new Exception("Database error"));
+
+        var result = await _controller.GetSessions();
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task GetSessions_ReturnsOk_WhenSessionsContainNullValues()
+    {
+        var sessions = new List<MeasurementSessionOverviewDto>
+        {
+            new MeasurementSessionOverviewDto
+            {
+                SessionId = 1,
+                DueDate = DateTime.Now.AddDays(1),
+                IsCompleted = false,
+                Requests = null
+            }
+        };
+
+        _serviceMock.Setup(s => s.GetSessionsByPatient(Patient1Id)).ReturnsAsync(sessions);
+
+        var result = await _controller.GetSessions();
 
         result.Should().BeOfType<OkObjectResult>();
     }
